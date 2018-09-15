0a3fc30151cc4b1c16e01cf7167fb4fb19417c68
MikeHannon <mjhannon@gmail.com>
2018-09-15 14:10:10 -0700

    adds ugly classification listing

--- src/components/taxonomic-tree.js
+++ src/components/taxonomic-tree.js
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import axios from 'axios';
 
+const CLASSIFICATIONS = [
+  'root',
+  'superkingdom',
+  'phylum',
+  'class',
+  'order',
+  'family',
+  'genus',
+  'species'
+]
 export default class TaxonomicTree extends React.Component {
-  state = {lineage:``}
+  state = {lineage:[]}
   componentDidMount(){
     this.updateTaxonInformation()
    
@@ -19,12 +29,12 @@ export default class TaxonomicTree extends React.Component {
     axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=${this.props.selectedTaxon.id}`).then(result=>{
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(result.data,"text/xml");
-      const lineage = xmlDoc.getElementsByTagName("Lineage")[0].childNodes[0].nodeValue;
-      this.setState((state)=>({...state, lineage }))
-    }).catch((e)=>{console.log(e)})
-
+      const lineage = xmlDoc.getElementsByTagName("Lineage")[0].childNodes[0].nodeValue.split(';');
+      this.setState((state)=>({...state, lineage }))}).catch((e)=>{console.log(e)})
   }
   render(){
-    return <p> {this.state.lineage} </p>
+  return <div>
+  {this.state.lineage.map((level, idx)=> (<p className={CLASSIFICATIONS[idx]} key={level+CLASSIFICATIONS[idx]}> {CLASSIFICATIONS[idx]} - {level} </p>) )}
+  </div>
   }
 }
